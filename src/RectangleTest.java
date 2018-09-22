1ea79f29c6d11a2e5732958441bd77be3b922823
247WIZ <247WIZ@247WIZONE.fios-router.home>
2018-09-22 16:50:09 -0400

    Test run

---
+++ src/RectangleTest.java
@@ -0,0 +1,13 @@
+import java.awt.Rectangle;
+
+public class RectangleTest {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Rectangle box = new Rectangle(10,20,30,40);
+		
+
+	}
+
+}
